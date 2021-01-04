5a7bcb61da18ca80952f9d2d32e31b45e1c40b52
Ademola Plumptre <ademola@overallheuristic.com>
2021-01-04 11:02:56 +0100

    update

--- resources/views/layouts/partials/logo.blade.php
+++ resources/views/layouts/partials/logo.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ asset('/')}}"><img class="img-responsive" src="{{ asset('logo_small.png')}}" alt="Logo"></a>
+<a href="{{ asset('/')}}"><img class="img-responsive" src="{{ asset('logo_small.png')}}" alt="Logo tfolc"></a>
 
